e3852a4e107299f49dd0564c78f14efb80cbb3a4
master <master.code.develop@gmail.com>
2022-12-21 00:35:14 +0100

    DAJR-31 update error page (JSX/SCSS)

--- frontend/src/pages/Error.jsx
+++ frontend/src/pages/Error.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Error() {
-  return <main>Error</main>;
+  return (
+    <main className="error-page">
+      <h1>404</h1>
+      <p>Oups! La page que vous demandez n&apos;existe pas.</p>
+      <Link to="/">Retourner sur la page d&apos;accueil</Link>
+    </main>
+  );
 }
