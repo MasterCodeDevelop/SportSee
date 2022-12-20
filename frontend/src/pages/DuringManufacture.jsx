7d3427c51afaf70d89fcd306ae74481cb6c0440c
master <master.code.develop@gmail.com>
2022-12-21 00:59:58 +0100

    DAJR-31 add DuringManufacture page (JSX/SCSS)

---
+++ frontend/src/pages/DuringManufacture.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function DuringManufacture({ page, message }) {
+  return (
+    <main className="error-page">
+      <h1>{page}</h1>
+      <p>{message} est en cours de fabrication</p>
+      <Link to="/">Retourner sur la page d&apos;accueil</Link>
+    </main>
+  );
+}
